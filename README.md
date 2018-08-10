6530d9160a0fba1549346ac8a1bd53882fab7b9a
Adam Szmyd <szmydadam@gmail.com>
2018-08-10 10:10:02 +0200

    Readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# Fake Backend
+
+## What for?
+
+Local development of various apps. It allows to dummy simple setup local server that serves contents.
+
+## Getting started
+
+Install deps:
+
+```bash
+npm i
+```
+
+Run:
+
+```bash
+npm start
+```
+
+It'll start you'r express server at http://localhost:3000
+
+See [./src/index.ts](./src/index.ts) for available REST paths
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,13 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
-app.get('/', (req, res) => res.send('Hello World!'));
-app.options('*', cors());
-
 const corsOptions = cors({
     origin: '*'
 });
 
+app.options('*', cors());
+
+app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/listeners', corsOptions, (req, res) => {
     return res.send(listeners(req));
 });
